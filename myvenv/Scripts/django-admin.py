21c9eb5d8921bbd6f140a54d0741ff5c336e93a4
github monotalk <okuda.takuya1223@gmail.com>
2020-09-28 23:06:43 +0900

    install django, making django_models

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\okuda\onedrive\desktop\django_demoapp\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
